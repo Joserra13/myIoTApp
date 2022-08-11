beb39cb499036372ebbd6c6bd8f9893e39578ba2
Joserra <Joserra_013@hotmail.com>
2022-08-11 22:41:07 +0200

    Add the websocket functionality to the app. Pending receive messages.

--- app/src/main/java/dte/masteriot/mdp/myiotapp2/MainActivity.java
+++ app/src/main/java/dte/masteriot/mdp/myiotapp2/MainActivity.java
@@ -1,29 +1,126 @@
 package dte.masteriot.mdp.myiotapp2;
 
+import static android.widget.Toast.LENGTH_LONG;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout item1;
-    LinearLayout item2;
+    private WebSocket webSocket;
+
+    TextView title;
+    Switch alarmSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        item1 = findViewById(R.id.Alarm);
-        item2 = findViewById(R.id.nDetections);
+        title = findViewById(R.id.textView);
+        alarmSwitch = findViewById(R.id.switch1);
 
-        ViewGroup.LayoutParams item1Params = item1.getLayoutParams();
-        ViewGroup.LayoutParams item2Params = item2.getLayoutParams();
+        instantiateWebSocket();
 
-        item2Params.height = item1Params.height;
+        alarmSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
 
-        item2.setLayoutParams(item2Params);
+                webSocket.send(String.valueOf(isChecked));
+                Toast.makeText(getApplicationContext(), "Msg sent!", LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private void instantiateWebSocket() {
+
+        OkHttpClient client = new OkHttpClient();
+
+        //replace x.x.x.x with your machine's IP Address
+        Request request = new Request.Builder().url("ws://192.168.1.58:8080").build();
+        SocketListener socketListener = new SocketListener(this);
+        webSocket = client.newWebSocket(request, socketListener);
+    }
+
+    public class SocketListener extends WebSocketListener {
+
+        public MainActivity activity;
+        public SocketListener(MainActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+
+            activity.runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    Toast.makeText(activity, "Connection Established!", LENGTH_LONG).show();
+                }
+            });
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, final String text) {
+
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                    title.setText(text);
+
+                    JSONObject jsonObject = new JSONObject();
+
+                    try {
+
+                        jsonObject.put("message", text);
+                        jsonObject.put("byServer", true);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            super.onMessage(webSocket, bytes);
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            super.onClosing(webSocket, code, reason);
+        }
+
+        @Override
+        public void onClosed(WebSocket webSocket, int code, String reason) {
+            super.onClosed(webSocket, code, reason);
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, final Throwable t, @Nullable final Response response) {
+            super.onFailure(webSocket, t, response);
+        }
     }
 }
